b4b8056c0b1f4a17e3d55412d3f75ae228cabb85
Juan David <juandavid0306@hotmail.com>
2021-03-12 16:07:58 -0500

    Add linters conf

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_03_12_204203) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

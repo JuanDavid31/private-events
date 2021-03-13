09fd910b75c8f605b9a9f29ace37a8724080b66d
Juan David <juandavid0306@hotmail.com>
2021-03-13 16:04:12 -0500

    Add Sign In and Sign Out features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+
+  root :to => 'users#index'
+
+  resources :users, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy'
+
 end
